305e65d7fc1276d7a9ca14e77708eb3e06802ee2
Jules-CLERC <48790000+Jules-CLERC@users.noreply.github.com>
2021-03-11 00:05:31 +0100

    Add gravity planet
    problem with grapple whit moving objects
    work on gravity

--- Grapple Attack Project/Assets/Scripts/PlayerMovement.cs
+++ Grapple Attack Project/Assets/Scripts/PlayerMovement.cs
@@ -9,8 +9,6 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D Rb;
     //Grap is a child object
     private GameObject Grap;
-    //diff between the image player and the real rotation
-    public float diffAngle = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -28,14 +26,6 @@ public class PlayerMovement : MonoBehaviour
             Rb.AddForce((target - transform.position).normalized * Speed);
             Debug.DrawLine(transform.position, target, Color.green);
         }
-        //TODO: problem with rotation
-        //ChangeSpaceShipRotation();
-    }
-
-    private void ChangeSpaceShipRotation()
-    {
-        float angle = Vector2.Angle(Rb.velocity.normalized, Vector2.right);
-        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, angle + diffAngle);
     }
 
     private Vector3 GetGrapPosition()
---
+++ Grapple Attack Project/Assets/Scripts/SkinRotation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkinRotation : MonoBehaviour
+{
+    //diff between the image player and the real rotation
+    public float diffAngle = 0f;
+    private Rigidbody2D RbPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RbPlayer = transform.parent.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeSpaceShipRotation();
+    }
+
+    private void ChangeSpaceShipRotation()
+    {
+        float angle = Vector2.Angle(RbPlayer.velocity.normalized, Vector2.right);
+        if (RbPlayer.velocity.normalized.y < 0)
+        {
+            angle = -angle;
+        }
+        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, angle + diffAngle);
+    }
+}
